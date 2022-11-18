e622a55961bdf97478ca3690004bac1ca40c8fa2
JesusProyect <jesusno.ven@gmail.com>
2022-11-18 02:18:34 +0100

    todo esta listo falta las sugerencias de capital

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 import { Country } from '../../interfaces/pais.interface';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+  `
+    li{
+      cursor: pointer;
+    }
+  
+  `
   ]
 })
 export class PorPaisComponent  {
 
   termino : string = '';
+  terminoSugerencia: string = '';
   hayError: boolean = false;
   paises  : Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
+  
 
   constructor( private paisService: PaisService ) { }
 
 
   buscar( termino: string ){
     this.hayError = false;
+    this.mostrarSugerencias = false;
+
+    if(this.termino === termino){
+      console.log('buscar')
+      return
+    }
     this.termino = termino;
 
     this.paisService.buscarPais( this.termino )
@@ -35,7 +52,27 @@ export class PorPaisComponent  {
 
   sugerencias( termino: string){
     this.hayError = false;
-    //TODO: crear sugerencias lo haremos en la siguiente seccion
+    if(this.terminoSugerencia === termino || termino.length === 0){
+      this.mostrarSugerencias = false;
+      return
+    }
+    this.terminoSugerencia = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais( termino )
+    .subscribe( 
+      { next: ( paises ) => {
+          this.paisesSugeridos = paises.splice(0,5) ;
+      },
+        error: (e) => {
+            this.paisesSugeridos = [];
+          }
+    })
+  }
+
+  buscarSugerido( termino: string ){
+    this.buscar( termino );
+  
   }
 
 
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button{
+      margin-right: 5px; 
+    }
+  `
   ]
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = [ 'EU', 'EFTA', 'CARICOM','PA', 'AU', 'USAN','EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'  ];
+  regionActiva: string = '';
+  paises: Country[] = [];
+  hayError: boolean = false;
+  termino: string = '';
+
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
 
+  activarRegion( region: string ){
+
+    if(region === this.regionActiva) {return}
+
+    this.regionActiva = region;
+    this.paises = [];
+    this.hayError = false;
+    //asi creo que esta deprecated no me queda claro creo 
+    this.paisService.buscarRegion(region)
+      .pipe( //esto no es necesario pero aja para tenerlo de ejemplo lo dejo
+        tap(console.log)
+      )
+      .subscribe( {//asi es la manera actual
+        next: (paises) => this.paises = paises,
+        error: (e) => {
+          this.hayError = true;
+          this.paises = [];
+        }
+      }
+      )
+     
+     
+  }
+
+  getClaseCss( region: string): string{
+    return (region === this.regionActiva) 
+              ? 'btn btn-primary' 
+              : 'btn btn-outline-primary '
+  }
 }
